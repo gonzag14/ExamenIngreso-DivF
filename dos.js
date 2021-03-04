ae253e6299f1b865c789dcb734c2d089d8950d35
gonzag14 <gagliardigonzalo@gmail.com>
2021-03-03 21:09:49 -0300

    punto 1 y 2 examen

--- dos.js
+++ dos.js
@@ -1,4 +1,96 @@
 function mostrar()
 {
-  alert("dos");
-}
+ let nombre
+ let situacionLaboral; 
+ let edad;
+ let cantidadMat;
+ let sexo
+ let seguir;
+ let promedio;
+ let NombreMejorP;
+ let mejorNota;
+ let flagNota = 0;
+ let nombreMV;
+ let edadMasViejo;
+ let flagMV = 0;
+ let promedio
+ let promedioBuscando;
+ let promedioTrabajando;
+ let promedioSoloE;
+ let acumBuscando = 0;
+ let acumTrabajando = 0;
+ let acumEstudiante = 0;
+let contBuscando = 0;
+let contTrabajando = 0;
+let contEstudiante = 0;
+let edadMenosMat;
+let menosMat;
+let nombreMenosMat
+let flagMaterias = 0;
+ do{
+  nombre = prompt("Ingrese el nombre: ");
+  situacionLaboral = prompt("Ingrese situacion laboral: ");
+    while(situacionLaboral != "buscando" && situacionLaboral != "trabajando" && situacionLaboral != "solo estudiante"){
+    situacionLaboral = prompt("Error. Ingrese situacion laboral: ");
+   }
+    sexo = prompt("Ingrese el sexo: ");
+    while(sexo != "m" || sexo != "f" || sexo != "no binario"){
+    sexo = prompt("Error. Ingrese el sexo: ");
+   }
+    cantidadMaterias = parseInt(prompt("Ingrese la cantidad de  materias: "));
+    while(cantidadMaterias < 1 && cantidadMaterias > 8) {
+    cantidadMaterias = parseInt(prompt("Error. Ingrese la cantidad de  materias: "));
+   }
+   promedio = parseInt(prompt("Ingrese nota promedio: "));
+   while(promedio < 0 && promedio > 10){
+   promedio = parseInt(prompt("Error. Ingrese nota promedio: "));
+   }
+   edad = parseInt(prompt("Ingrese edad: "));
+
+  if(flagNota == 0 || mejorNota < promedio && situacionLaboral == "buscando" && situacionLaboral == "trabajando"){
+    mejorNota = promedio
+    flagNota = 1;
+  }
+ 
+ 
+ 
+  if(flagMV == 0 || edad > nombreMV && situacionLaboral == "estudiante")
+  edadMasViejo = edad
+  nombreMV = nombre
+  }
+
+  if(situacionLaboral == "buscando"){
+    acumBuscando += mejorNota
+    contBuscando++
+    }
+  else if(situacionLaboral == "trabajando"){
+    acumBuscando += mejorNota
+    contBuscando++
+    }
+   else{
+   acumEstudiante += mejorNota
+   contEstudiante++
+  }
+  
+  if(flagMaterias == 0 || menosMat < cantidadMat && situacionLaboral == "trabajando"){
+    menosMat =cantidadMat;
+    edadMenosMat = edad
+    nombreMenosMat = nombre
+  }
+  
+  
+  
+  seguir = prompt("Quiere ingresar mas alumnos?")
+  }
+while(seguir == "s");
+
+promedioTrabajando = acumTrabajando / contTrabajando;
+promedioSoloE = acumEstudiante / contEstudiante;
+promedioBuscando = acumEstudiante / contEstudiante;
+
+
+console.log("" + promedio)
+console.log( nombreMV)
+console.log("Mejor promedio estudiante " + promedioSoloE + " mejor promedio trabajando " + promedioTrabajando + " mejor promedio buscando " + promedioBuscando)
+console.log("La edad del que menos cursa es de " edadMenosMat + " y su nombre es " + nombreMenosMat)
+
--- uno.js
+++ uno.js
@@ -1,5 +1,142 @@
 
 function mostrar()
 {
-	alert("uno");
+	let producto;
+	let precio;
+	let cantidad;
+	let categoria;
+	let fabricante;
+	let promedioAl;
+	let promedioIa;
+	let promedioQu;
+	let acumAl = 0;
+	let acumIa = 0;
+	let acumQu = 0;
+	let contAl= 0;
+	let contIa = 0;
+	let contQu = 0;
+	let catMayor;
+	let unidadesDet;
+	let contDesinf = 0;
+	let contBact = 0;
+	let contDet = 0;
+	let acumDesinf = 0;
+	let acumBact = 0;
+	let acumDet = 0;
+	let cantidadCat;
+	let UnidadesDet;
+	let fabricanteCaro;
+	let categoriaCaro;
+	let flagFabricante = 0;
+	let flagCategoria = 0;
+	
+  
+  
+	for ( let i = 0; i < 5 ; i++ ){
+  
+		producto = prompt("Ingrese produto: iac, alcohol o quat.");
+		while(producto != "ALCOHOL" & producto != "IAC" && producto != "QUAT"){
+			producto = prompt("Error. Ingrese producto: ")
+		}
+  
+		precio = parseFloat(prompt("Ingrese precio entre $100 y $300"))
+		while (precio < 100 || precio > 300 || isNaN(precio) ){				
+			precio = prompt("Error. Ingrese precio entre $100 y $300");
+		}
+		cantidad = parseInt(prompt("Ingrese cantidad"))
+		while(   !(cantidad > 0 && cantidad <= 1000)  ){
+			cantidad = prompt("Error. Ingrese cantidad max 1000")
+		}
+		categoria = prompt("Ingrese tipo: desinfectante, bactericida o detergente.");
+		while(categoria != ("desinfectante") & categoria != ("bactericida") && categoria != ("detergente")){
+			categoria = prompt("Error. Ingrese categoria: ")
+		}
+		fabricante = prompt("ingrese fabricante");
+		
+		
+		switch(producto){
+			case "ALCOHOL":
+			acumAl += cantidad
+			contAl++;
+			break;
+			case "IAC":
+			acumIa += cantidad
+			contIa++
+			break;
+			case "QUAT":
+			acumQu += cantidad;
+			contQu++
+
+			break;
+
+		}
+	
+	  
+	}
+
+	if(contAl != 0 ){
+		promedioAl = acumAl / contAl; 
+	}
+	if(contIa != 0){
+		promedioIa = acumIa / contIa;;
+	}
+	if(contQu != 0){
+		promedioQu = acumQu / contQu;
+	}
+
+	
+	
+	switch(categoria){
+		case "DETERGENTE":
+		acumDet += cantidadCat
+		contDet++;
+		break;
+		case "desinfectante":
+		acumDesinf += cantidadCat
+		contDesinf++
+		break;
+		case "bactericida":
+		acumQu += cantidadCat;
+		contBact++
+
+		break;
+
+	}
+	if(acumDet > acumDesinf && acumDet > acumBact ){
+		//acumalcohol sea mayor
+			catMayor = "alcohol"
+		
+		
+		}
+		else if( acumIa > acumAl  &&  acumIa >= acumQu ){
+		
+			catMayor = "IAC"
+			
+		}
+		
+		else{
+		
+			catMayor = "QUAT"
+		
+	}
+	
+
+	if(precio <= 200 && categoria == "detergente"){
+		unidadesDet = contDet
+	}
+
+	
+
+
+
+	console.log("Promedio de alcohol" + promedioAl + " \nPromedio IAC " + promedioIa + " \nPromedio QUAT " + promedioQu )
+	console.log("a categoria con mas cantidad de unidades en total de la compra "+ catMayor)
+	console.log("unidades de detergente con precios menos a 200" + unidadesDet)
+	
+
 }
+/*a) El promedio de cantidad por tipo de producto
+b) La categoria con mas cantidad de unidades en total de la compra
+c) Cuántas unidades de detergente con precios menos a 200 (inclusive) se compraron en total
+d) el fabricante y Categoria del más caro de los productos 
+*/
